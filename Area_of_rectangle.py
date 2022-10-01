649899e06bccf146c723a7013586f5ce166f666d
Prathmesh Raut <96947384+Prathmesh2312@users.noreply.github.com>
2022-10-01 23:41:12 +0530

    Simple Programs

---
+++ Area_of_rectangle.py
@@ -0,0 +1,5 @@
+# Program to find the area of Rectangle
+l=float(input("Enter the length of Rectangle:"))
+b=float(input("Enter the breadth of Rectangle:"))
+area=l*b
+print("Area of Rectangle is:",area)
---
+++ Swap_Value_of_two_variables.py
@@ -0,0 +1,13 @@
+#Program to swap the value of two variables
+
+num1=input("Enetr first number:")
+num2=input("Enter second number:")
+print("Numbers before swapping")
+print("num1:",num1)
+print("num2:",num2)
+temp=num1
+num1=num2
+num2=temp
+print("Numbers after swapping")
+print("num1:",num1)
+print("num2:",num2)
