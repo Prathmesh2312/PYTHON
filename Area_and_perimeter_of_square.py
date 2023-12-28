0ea366b9399f9e14d80304a90084cd40c92c0e29
Prathmesh Raut <96947384+Prathmesh2312@users.noreply.github.com>
2023-12-28 21:31:17 +0530

    Delete Area_and_perimeter_of_square.py

--- Area_and_perimeter_of_square.py
+++
@@ -1,6 +0,0 @@
-#Program to calculate the area and parameter of square
-side=float(input("Enter the side length of square:"))
-area=side*side
-perimeter=4*side
-print("Area of Square is:",area)
-print("Perimeter of square is:",perimeter)
