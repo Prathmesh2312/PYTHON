fd47ae1a8579a0bfbbb43f8cd722a718dd7a6928
Prathmesh Raut <96947384+Prathmesh2312@users.noreply.github.com>
2023-12-28 21:31:29 +0530

    Delete Area_of_rectangle.py

--- Area_of_rectangle.py
+++
@@ -1,5 +0,0 @@
-# Program to find the area of Rectangle
-l=float(input("Enter the length of Rectangle:"))
-b=float(input("Enter the breadth of Rectangle:"))
-area=l*b
-print("Area of Rectangle is:",area)
